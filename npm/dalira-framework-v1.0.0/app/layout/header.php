7ce4aba5ad3895d98080279410648358f8ba1799
appworksco <info.appworksco@gmail.com>
2024-10-20 16:52:24 +0800

    Updated

---
+++ npm/dalira-framework-v1.0.0/app/layout/header.php
@@ -0,0 +1,32 @@
+<?php
+
+// Start session management and output buffering
+session_start();
+ob_start();
+
+// Array to store messages
+$invalid = array();
+$success = array();
+$warning = array();
+$info = array();
+ 
+// Include necessary files for database connectivity and facade classes
+include(__DIR__ . '/../../config/db/connector.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Appworks Co.">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.4/css/dataTables.dataTables.min.css">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <title>Dalira Framework | A lightweight PHP starter template</title>
+</head>
+
+<body>
